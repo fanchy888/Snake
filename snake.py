dbd8adfaf42a680c68871a4e89774df3fdf6cf71
Chunyu Fan <fanchy888@gmail.com>
2017-09-16 21:10:17 -0400

    add menu

--- snake.py
+++ snake.py
@@ -5,9 +5,8 @@ from pygame.locals import *
 from sys import exit
 import random
 
-
 class Snake(object):
-	def __init__(self,image):
+	def __init__(self,image,mode):
 		self.image=image
 		self.lenth=0
 		self.body=[Vector2(30,360)]
@@ -15,21 +14,25 @@ class Snake(object):
 		self.eat=False
 		self.dead=False
 		self.lock=False
+		self.game_mode=mode
 	def move(self,food):
 		self.check(food)
-		head=self.body[0]
-		head+=self.direction*30
-		self.body.insert(0,head)
-		if not self.eat:
-			self.body.pop()
-		if self.body[0].x<0:
-			self.body[0].x=1050
-		if self.body[0].x>=1080:
-			self.body[0].x=0
-		if self.body[0].y<0:
-			self.body[0].y=690
-		if self.body[0].y>=720:
-			self.body[0].y=0
+		if not self.dead:
+			head=self.body[0]
+			head+=self.direction*30
+			self.body.insert(0,head)
+			if not self.eat:
+				self.body.pop()
+			if self.game_mode:
+				if self.body[0].x<0:
+					self.body[0].x=1050
+				if self.body[0].x>=1080:
+					self.body[0].x=0
+				if self.body[0].y<0:
+					self.body[0].y=690
+				if self.body[0].y>=720:
+					self.body[0].y=0
+
 	def check(self,food):
 		if self.body[0]==food:
 			self.eat=True
@@ -38,6 +41,12 @@ class Snake(object):
 			self.eat=False
 		if self.body[0] in self.body[1:]:
 			self.dead=True
+		if (self.body[0].x==0 and self.direction==(-1,0)) or \
+			(self.body[0].x==1050 and self.direction==(1,0)) or\
+			(self.body[0].y==0 and self.direction==(0,-1)) or \
+			(self.body[0].y==690 and self.direction==(0,1)):
+			if not self.game_mode:
+				self.dead=True
 	def change(self,key):
 		new_d=self.direction
 
@@ -80,9 +89,9 @@ screen_size=Vector2(1080,720)
 pygame.init()
 screen=pygame.display.set_mode(screen_size,0,32)
 pygame.display.set_caption('snake')
-snake_image=pygame.image.load('snake.png').convert()
-food_image=pygame.image.load('food.png').convert()
-head_image=pygame.image.load('head.png').convert()
+
+
+
 clock=pygame.time.Clock()
 
 
@@ -91,13 +100,138 @@ score_font=pygame.font.SysFont("楷体",40)
 gameover=font.render("GAME OVER",True,(0,0,0))
 retry_font=pygame.font.SysFont("楷体",40)
 retry=retry_font.render("Press 'r' to play again",True,(0,0,0))
-def main():
-	global snake,food
-	snake=Snake(snake_image)
+
+black=(0,0,0)
+white=(255,255,255)
+def new_game():
+	global snake,food,game_mode
+	load_setting()
+	snake=Snake(snake_image,game_mode)
 	food=Food(food_image,snake)
-main()
-while True:
+	
+def menu(mouse,button,names):
+	global settings
+	num=len(names)	
+	for i in range(num):
+		if state_stack[-1]=='color':
+			text=pygame.image.load(names[i]+'.png').convert()
+		else:
+			text=font.render(names[i],True,black)
+		w,h=text.get_size()
+		position=(540-w/2,330-h+(i-num/2+1)*100)
+		block=Rect(position,(w,h))
+		mouse_in_blocks=in_rect(block,mouse)
+		if (state_stack[-1] in settings) and settings[state_stack[-1]]==names[i]:
+			display(text,block,mouse_in_blocks,True)
+		else:
+			display(text,block,mouse_in_blocks)
+		if button and mouse_in_blocks:
+			click(names[i],(state_stack[-1] in settings))		
+			
+def display(text,block,mouse_in_block,mark=False):
+	pos=(block[0],block[1])
+	if mark:
+		sub=(block[0]-3,block[1]-3,block[2]+5,block[3]+5)
+		pygame.draw.rect(screen,(80,80,80),sub,4)
+	if mouse_in_block:
+		pygame.draw.rect(screen,black,block,3)
+	screen.blit(text,pos)
+	
+def click(name,set):
+	if name=='start':
+		new_game()
+		state_stack.append('game')	
+	if name=='option':
+		state_stack.append('option')
+	if name=='mode':
+		state_stack.append('mode')
+	if name=='speed':
+		state_stack.append('speed')
+	if name=='color':
+		state_stack.append('color')
+
+	if set:
+		settings[state_stack[-1]]=name
+		
+def in_game():
 	snake.lock=False
+	if not game_mode:
+		pygame.draw.rect(screen,(0,0,0),(0,0,1080,720),10)
+	if not snake.dead:
+		snake.move(food.location)
+		food.check(snake)
+	snake.display(screen,head_image)
+	food.display(screen)
+	if snake.dead:
+		screen.blit(gameover,(400,300))		
+		screen.blit(retry,(430,350))
+	score=score_font.render("score:"+str(snake.lenth),True,(0,0,0))
+	screen.blit(score,(0,0))
+		
+def in_rect(rect,point):
+	if (point[0]<rect[0]+rect[2] and point[0]>rect[0]) and \
+		(point[1]<rect[1]+rect[3] and point[1]>rect[1]):
+		return True
+	else:
+		return False
+
+state_stack=[]
+state_stack.append('start')
+settings={}
+settings['mode']='no wall'
+settings['speed']='10'
+settings['color']='snake'
+speed=60
+def load_setting():
+	global speed,game_mode,snake_image,head_image,food_image
+	speed=int(settings['speed'])
+	if settings['mode']=='wall':
+		game_mode=False
+	else:
+		game_mode=True
+	snake_image=pygame.image.load(settings['color']+'.png').convert()
+	head_image=pygame.image.load(settings['color']+'_head.png').convert()
+	food_image=pygame.image.load('food.png').convert()
+	
+def back(mouse,button):
+	color=black
+	width=2
+	if in_rect((960,600,80,50),mouse):
+		color=white
+		width=0
+		if button:
+			state_stack.pop()	
+	pygame.draw.rect(screen,(0,0,0),(960,600,80,50),width)
+	back=score_font.render('back',True,color)
+	screen.blit(back,(970,610))
+	
+def main(mouse,button):
+	global state_stack
+	state_id=state_stack[-1]
+	go_back=False
+	if state_id=='start':
+		go_back=False
+		menu(mouse,button,['start','option'])		
+	if state_id=='option':
+		go_back=True
+		menu(mouse,button,['mode','speed','color'])
+	if state_id=='game':
+		in_game()
+	if state_id=='mode':
+		go_back=True
+		menu(mouse,button,['wall','no wall'])
+	if state_id=='speed':
+		go_back=True
+		menu(mouse,button,['5','10','15','20','25'])
+	if state_id=='color':
+		go_back=True
+		menu(mouse,button,['red','snake','blue1','grey','yellow'])		
+	if go_back:
+		back(mouse,button)
+		
+while True:
+	buttons=pygame.mouse.get_pressed()
+	left=False
 	for event in pygame.event.get():
 		if event.type==QUIT:
 			exit()			
@@ -107,22 +241,16 @@ while True:
 			if event.key in (K_UP,K_DOWN,K_LEFT,K_RIGHT):
 				snake.change(event.key)
 			if event.key==K_r:
-				main()
+				if len(state_stack)>1:
+					state_stack.pop()
+					speed=60
+		if event.type==pygame.MOUSEBUTTONUP and buttons[0]:
+			left=True
+	mouse_position=pygame.mouse.get_pos()
+	time=clock.tick(speed)
 
-	time=clock.tick(20)
 	screen.fill((255,255,255))
-
-	if not snake.dead:
-		snake.move(food.location)
-		food.check(snake)
-	snake.display(screen,head_image)
-	food.display(screen)
-	if snake.dead:
-		screen.blit(gameover,(400,300))		
-		screen.blit(retry,(430,350))	
-	score=score_font.render("score:"+str(snake.lenth),True,(0,0,0))
-	screen.blit(score,(0,0))
-
+	main(mouse_position,left)
 	pygame.display.update()
 
 
